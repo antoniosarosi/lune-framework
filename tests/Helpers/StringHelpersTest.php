9e198ee4dbfe68410a2b0efa6a807311c5d9ac53
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-12 19:29:02 -0500

    Add: camel case string function helper test

--- tests/Helpers/StringHelpersTest.php
+++ tests/Helpers/StringHelpersTest.php
@@ -34,10 +34,46 @@ class StringHelpersTest extends TestCase {
         ];
     }
 
+    public function provideCamelCaseData() {
+        return [
+            [
+                "camel_case_word",
+                "camelCaseWord"
+            ],
+            [
+                "SomeClassName",
+                "someClassName"
+            ],
+            [
+                "String with    spaces",
+                "stringWithSpaces"
+            ],
+            [
+                "   String with    leading and trailing  spaces   ",
+                "stringWithLeadingAndTrailingSpaces"
+            ],
+            [
+                "string___with---hyphens__and-underscores",
+                "stringWithHyphensAndUnderscores"
+            ],
+            [
+                "  String   with  spaces ___ and ---snake_case and ___Camel---Case_with_SnakeCase  ",
+                "stringWithSpacesAndSnakeCaseAndCamelCaseWithSnakeCase"
+            ]
+        ];
+    }
+
     /**
      * @dataProvider strings
      */
     public function testSnakeCase($test, $expected) {
         $this->assertEquals($expected, snake_case($test));
+    }
+
+    /**
+     * @dataProvider provideCamelCaseData
+     */
+    public function testCamelCase($test, $expected) {
+        $this->assertEquals($expected, camel_case($test));
     }
 }
